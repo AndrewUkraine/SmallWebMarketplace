0ca76aad5e97524b16b7cf5e1b38e7127ea2e138
AndrewUkraine <andrewukraine17@gmail.com>
2019-03-11 14:34:24 +0200

    add path to photos

--- src/main/java/com/marketplace/project/controllers/GlobalExceptionHandler.java
+++ src/main/java/com/marketplace/project/controllers/GlobalExceptionHandler.java
@@ -1,12 +1,20 @@
 package com.marketplace.project.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.multipart.MultipartException;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @ControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler implements WebMvcConfigurer {
+
+    @Value("${upload.path}")
+    private String uploadPath;
 
     @ExceptionHandler(MultipartException.class)
     public String handleError1(MultipartException e, RedirectAttributes redirectAttributes) {
@@ -14,5 +22,13 @@ public class GlobalExceptionHandler {
         redirectAttributes.addFlashAttribute("message", e.getCause().getMessage());
         return "redirect:/uploadStatus";
 
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations("file://" + uploadPath + "/");
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/");
     }
 }
